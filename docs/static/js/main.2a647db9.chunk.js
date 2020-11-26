618e11e7b3d9e5417cbc56329b0b9b56489a08ad
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2020-11-26 11:48:34 -0600

    Set up basic project structure, including Sass

---
+++ docs/static/js/main.2a647db9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpronquilloaeon=this.webpackJsonpronquilloaeon||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var c=t(0),r=t(1),o=t.n(r),s=t(3),a=t.n(s);t(9);var i=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("header",{className:"App-header"})})};a.a.render(Object(c.jsx)(o.a.StrictMode,{children:Object(c.jsx)(i,{})}),document.getElementById("root"))},9:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.2a647db9.chunk.js.map
---
+++ src/components/projects/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Projects() {
+    return (
+        <div className="section">
+            <div id="projects" className="container">
+                <div className="row">
+                    <div className="col">
+                        <header>
+                            <h2 className="color-champagne">Projects</h2>
+                        </header>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
